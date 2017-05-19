da153fd374881cf4310baf5ca1aac1969d78f639
Jérémy Simonklein <jeremy-s@equivote.fr>
2017-05-19 19:48:31 +0200

    SigninForm and handling signIn in Navbar

--- imports/client/general/ui/NavbarAccountItem.jsx
+++ imports/client/general/ui/NavbarAccountItem.jsx
@@ -2,6 +2,7 @@ import {Meteor} from 'meteor/meteor'
 import React, {Component} from 'react'
 import TrackerReact from 'meteor/ultimatejs:tracker-react'
 import {Menu, Modal} from 'semantic-ui-react'
+import SigninForm from '/imports/client/accounts/ui/SigninForm'
 
 export default class NavbarAccountItem extends TrackerReact(Component){
 
@@ -17,6 +18,17 @@ export default class NavbarAccountItem extends TrackerReact(Component){
     }
   }
 
+  onSignin(user_id){
+    FlowRouter.go('Landing')
+  }
+
+  toggleState(attr, e){
+    e.preventDefault()
+    let state = this.state
+    state[attr] = !state[attr]
+    this.setState(state)
+  }
+
   render(){
     const current_user = Meteor.user()
 
@@ -25,7 +37,19 @@ export default class NavbarAccountItem extends TrackerReact(Component){
         <Menu.Item className="navbar-item">{current_user.username}</Menu.Item>
         )
     }else{
-      return <Menu.Item className="navbar-item" name="Connexion"/>
+      return (
+        <div>
+          <Menu.Item className="navbar-item" onClick={(e) => {this.toggleState('open_modal', e)}} name="Connexion"/>
+          <Modal open={this.state.open_modal}>
+            <Modal.Header>Connexion</Modal.Header>
+            <Modal.Content>
+              <Modal.Description>
+                <SigninForm onSignin={this.onSignin.bind(this)} />
+              </Modal.Description>
+            </Modal.Content>
+          </Modal>
+        </div>
+      )
     }
   }
 }
