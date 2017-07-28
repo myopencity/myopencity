72e11e0dffa7d9b12c504b6a3ffc47141d2039e7
Jérémy Simonklein <jeremy-s@equivote.fr>
2017-07-28 15:40:57 +0200

    Added handling of Amazon upload if configured

--- imports/api/external_apis_configuration/server/methods.js
+++ imports/api/external_apis_configuration/server/methods.js
@@ -1,6 +1,7 @@
 import {Meteor} from 'meteor/meteor'
 import {ExternalApisConfiguration} from '../external_apis_configuration'
 import {Configuration} from '/imports/api/configuration/configuration'
+import _ from 'lodash'
 
 Meteor.methods({
   'external_apis_configuration.amazon_update'({amazon_public_key, amazon_private_key}){
@@ -10,6 +11,31 @@ Meteor.methods({
       ExternalApisConfiguration.update({}, {$set: {amazon_public_key, amazon_private_key}})
       if(amazon_public_key, amazon_private_key){
         Configuration.update({}, {$set: {amazon_connected: true}})
+        Slingshot.createDirective("ConsultImage", Slingshot.S3Storage, {
+          bucket: "myopencity",
+          acl: "public-read",
+          AWSAccessKeyId: amazon_public_key,
+          AWSSecretAccessKey: amazon_private_key,
+          region: 'eu-central-1',
+
+          authorize: function (file, metaContext) {
+            if(!this.userId){
+              throw new Meteor.Error('403', "Vous devez vous connecter")
+            }else{
+              return true
+            }
+          },
+
+          key: function (file, metaContext) {
+            // User's image url with ._id attached:
+            console.log("metacontext", metaContext);
+            const fileNameDecompo = _.split(file.name, '.')
+            const url = "images/" + this.userId + "/" + Date.now() + "-" + _.kebabCase(fileNameDecompo[0]) + '.' + fileNameDecompo[fileNameDecompo.length - 1]
+            console.log("URL", url);
+
+            return url
+          }
+        })
       }
     }
   },
