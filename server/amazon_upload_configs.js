77bb103bfbefb260679a87beb5d690c7b349604f
Jérémy Simonklein <jeremy-s@equivote.fr>
2017-08-04 13:17:16 +0200

    Corrected amazon initial config if configuration doesn't exist

--- server/amazon_upload_configs.js
+++ server/amazon_upload_configs.js
@@ -14,56 +14,61 @@ Slingshot.fileRestrictions("ConsultDocument", {
   maxSize: null
 })
 
-if(external_apis_conf.amazon_public_key && external_apis_conf.amazon_private_key){
-  Slingshot.createDirective("ConsultImage", Slingshot.S3Storage, {
-    bucket: "myopencity",
-    acl: "public-read",
-    AWSAccessKeyId: external_apis_conf.amazon_public_key,
-    AWSSecretAccessKey: external_apis_conf.amazon_private_key,
-    region: 'eu-central-1',
+if(external_apis_conf){
+  if(external_apis_conf.amazon_public_key && external_apis_conf.amazon_private_key){
+    Slingshot.createDirective("ConsultImage", Slingshot.S3Storage, {
+      bucket: "myopencity",
+      acl: "public-read",
+      AWSAccessKeyId: external_apis_conf.amazon_public_key,
+      AWSSecretAccessKey: external_apis_conf.amazon_private_key,
+      region: 'eu-central-1',
 
-    authorize: function (file, metaContext) {
-      if(!this.userId){
-        throw new Meteor.Error('403', "Vous devez vous connecter")
-      }else{
-        return true
+      authorize: function (file, metaContext) {
+        if(!this.userId){
+          throw new Meteor.Error('403', "Vous devez vous connecter")
+        }else{
+          return true
+        }
+      },
+
+      key: function (file, metaContext) {
+        // User's image url with ._id attached:
+        console.log("metacontext", metaContext);
+        const fileNameDecompo = _.split(file.name, '.')
+        const url = "images/" + this.userId + "/" + Date.now() + "-" + _.kebabCase(fileNameDecompo[0]) + '.' + fileNameDecompo[fileNameDecompo.length - 1]
+        console.log("URL", url);
+
+        return url
       }
-    },
+    })
 
-    key: function (file, metaContext) {
-      // User's image url with ._id attached:
-      console.log("metacontext", metaContext);
-      const fileNameDecompo = _.split(file.name, '.')
-      const url = "images/" + this.userId + "/" + Date.now() + "-" + _.kebabCase(fileNameDecompo[0]) + '.' + fileNameDecompo[fileNameDecompo.length - 1]
-      console.log("URL", url);
+    Slingshot.createDirective("ConsultDocument", Slingshot.S3Storage, {
+      bucket: "myopencity",
+      acl: "public-read",
+      AWSAccessKeyId: external_apis_conf.amazon_public_key,
+      AWSSecretAccessKey: external_apis_conf.amazon_private_key,
+      region: 'eu-central-1',
 
-      return url
-    }
-  })
+      authorize: function (file, metaContext) {
+        if(!this.userId){
+          throw new Meteor.Error('403', "Vous devez vous connecter")
+        }else{
+          return true
+        }
+      },
 
-  Slingshot.createDirective("ConsultDocument", Slingshot.S3Storage, {
-    bucket: "myopencity",
-    acl: "public-read",
-    AWSAccessKeyId: external_apis_conf.amazon_public_key,
-    AWSSecretAccessKey: external_apis_conf.amazon_private_key,
-    region: 'eu-central-1',
+      key: function (file, metaContext) {
+        // User's image url with ._id attached:
+        console.log("metacontext", metaContext);
+        const fileNameDecompo = _.split(file.name, '.')
+        const url = "documents/" + this.userId + "/" + Date.now() + "-" + _.kebabCase(fileNameDecompo[0]) + '.' + fileNameDecompo[fileNameDecompo.length - 1]
+        console.log("URL", url);
 
-    authorize: function (file, metaContext) {
-      if(!this.userId){
-        throw new Meteor.Error('403', "Vous devez vous connecter")
-      }else{
-        return true
+        return url
       }
-    },
+    })
+  }
+}else{
+  console.log("SERVER : Slingshot didn't initialized directives, cause no configuration has been found");
 
-    key: function (file, metaContext) {
-      // User's image url with ._id attached:
-      console.log("metacontext", metaContext);
-      const fileNameDecompo = _.split(file.name, '.')
-      const url = "documents/" + this.userId + "/" + Date.now() + "-" + _.kebabCase(fileNameDecompo[0]) + '.' + fileNameDecompo[fileNameDecompo.length - 1]
-      console.log("URL", url);
-
-      return url
-    }
-  })
 }
