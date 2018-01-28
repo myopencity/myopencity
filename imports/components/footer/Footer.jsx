11a69e159a8b6fb64ae0c34fe75f236d23c126cd
Jérémy Simonklein <jeremy-s@equivote.fr>
2018-01-28 20:54:56 +0100

    Modified design of footer

--- imports/components/footer/Footer.jsx
+++ imports/components/footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Container } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 export default class Footer extends Component {
@@ -8,9 +8,11 @@ export default class Footer extends Component {
 
         return (
             <Grid stackable className="footer" verticalAlign="middle" style={{ backgroundColor: footer_background_color, color: footer_color, height: footer_height }}>
-                <Grid.Column width={16} className="center-align" >
-                    <p>{footer_content}</p>
-                    {footer_cgu_display && <Link to='/conditions' className="pointer"><p>{cgu_term}</p></Link>}
+                <Grid.Column width={16}>
+                    <Container>
+                        <span className="left-align">{footer_content}</span>
+                        {footer_cgu_display && <Link to='/conditions' className="pointer" style={{float: 'right'}}><span>{cgu_term}</span></Link>}
+                    </Container>
                 </Grid.Column>
             </Grid>
         );
